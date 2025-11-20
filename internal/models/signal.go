16862d1b8d3718c14f7d9447136d039b8b2c508a
Prasanth Thiagarajan <prasanth33460@gmail.com>
2025-11-21 02:53:54 +0530

    fix: added specific proper structs for models.

--- internal/models/signal.go
+++ internal/models/signal.go
@@ -3,16 +3,16 @@ package models
 type SignalType string
 
 const (
-	AdRequest    SignalType = "AdRequest"
-	AdResponse   SignalType = "AdResponse"
-	AdImpression SignalType = "AdImpression"
-	AdClick      SignalType = "AdClick"
+	AdRequest    SignalType = "adRequest"
+	AdResponse   SignalType = "adResponse"
+	AdImpression SignalType = "adImpression"
+	AdClick      SignalType = "adClick"
 )
 
 type SignalRequest struct {
 	App     string     `json:"app" validate:"required"`
 	Country string     `json:"country" validate:"required"`
-	Signal  SignalType `json:"signal" validate:"required"`
+	Signal  SignalType `json:"signal" validate:"required,oneof=adRequest adResponse adImpression adClick"`
 	UserID  string     `json:"user_id,omitempty"`
 }
 
@@ -24,7 +24,7 @@ type DimensionKey struct {
 
 type AggregatedMetrics struct {
 	AdRequest    int64
-	Adresponse   int64
+	AdResponse   int64
 	AdImpression int64
 	AdClick      int64
 	UniqueUsers  map[string]struct{}
@@ -33,7 +33,7 @@ type AggregatedMetrics struct {
 type DBRecord struct {
 	Date         string
 	App          string
-	Coutnry      string
+	Country      string
 	AdRequest    int64
 	AdResponse   int64
 	AdImpression int64

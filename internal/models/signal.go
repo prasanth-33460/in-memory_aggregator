089975279b16c69d50874e9750a70661fb9da54c
Prasanth Thiagarajan <prasanth33460@gmail.com>
2025-11-21 00:17:30 +0530

    feat: add signal request and aggregation models

--- internal/models/signal.go
+++ internal/models/signal.go
@@ -1,1 +1,43 @@
-package signal
+package models
+
+type SignalType string
+
+const (
+	AdRequest    SignalType = "AdRequest"
+	AdResponse   SignalType = "AdResponse"
+	AdImpression SignalType = "AdImpression"
+	AdClick      SignalType = "AdClick"
+)
+
+type SignalRequest struct {
+	App     string     `json:"app" validate:"required"`
+	Country string     `json:"country" validate:"required"`
+	Signal  SignalType `json:"signal" validate:"required"`
+	UserID  string     `json:"user_id,omitempty"`
+}
+
+type DimensionKey struct {
+	Date    string
+	App     string
+	Country string
+}
+
+type AggregatedMetrics struct {
+	AdRequest    int64
+	Adresponse   int64
+	AdImpression int64
+	AdClick      int64
+	UniqueUsers  map[string]struct{}
+}
+
+type DBRecord struct {
+	Date         string
+	App          string
+	Coutnry      string
+	AdRequest    int64
+	AdResponse   int64
+	AdImpression int64
+	AdClick      int64
+	DAU          int64
+	CreatedAt    int64
+}

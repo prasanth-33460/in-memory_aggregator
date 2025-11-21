69a204e7189aba1c0d2577dcc5cf772261d07e40
Prasanth Thiagarajan <prasanth33460@gmail.com>
2025-11-21 17:35:28 +0530

    feat: added handler with health and post of ingest

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,80 @@
 package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/prasanth-33460/in-memory_aggregator/internal/models"
+	aggregator "github.com/prasanth-33460/in-memory_aggregator/internal/service"
+)
+
+type Handler struct {
+	aggregator *aggregator.Aggregator
+}
+
+func NewHandler(agg *aggregator.Aggregator) *Handler {
+	return &Handler{
+		aggregator: agg,
+	}
+}
+
+func (h *Handler) IngestSignal(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed. ", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req models.SignalRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Printf("failed to decode JSON: %v", err)
+		http.Error(w, "invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+
+	if req.App == "" {
+		http.Error(w, "missing required field: app", http.StatusBadRequest)
+		return
+	}
+	if req.Country == "" {
+		http.Error(w, "missing required field: country", http.StatusBadRequest)
+		return
+	}
+	if req.Signal == "" {
+		http.Error(w, "missing required field: signal", http.StatusBadRequest)
+		return
+	}
+
+	validSignals := map[models.SignalType]bool{
+		models.AdRequest:    true,
+		models.AdResponse:   true,
+		models.AdImpression: true,
+		models.AdClick:      true,
+	}
+
+	if !validSignals[req.Signal] {
+		http.Error(w, "invalid signal type. must be one type of the 4 signals.", http.StatusBadRequest)
+		return
+	}
+
+	date := time.Now().Format("2000-01-01") //year-mm-dd
+
+	h.aggregator.Add(req, date)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	stats := h.aggregator.GetStats()
+	response := map[string]any{
+		"status":          "healthy",
+		"total_requests":  stats.TotalRequests,
+		"current_size":    stats.CurrentSize,
+		"last_flush_time": stats.LastFlushTime.Format(time.RFC3339),
+		"uptime_seconds":  time.Since(stats.LastFlushTime).Seconds(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
